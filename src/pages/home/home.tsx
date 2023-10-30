f619bcbc40555bd541b763b613581e3feb4d7913
Untra <87949578+Azizho@users.noreply.github.com>
2023-10-30 17:44:34 +0300

    Update home.tsx

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,8 +1,8 @@
 'use client'
 
-import { AuthCheck } from '@/components/auth-check'
+import AuthCheck  from '@/components/auth-check'
 import React from 'react'
-import { HomeBody } from './components/body/body'
+import HomeBody from './components/body/body'
 
 const HomePage = () => {
 	return (
